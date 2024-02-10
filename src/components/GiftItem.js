3b73e05521cc876a158f8203356f5dff835d87ad
luisbrito2900 <luisjahziel1029@gmail.com>
2024-02-09 20:07:27 -0400

    removing hover

--- src/components/GiftItem.js
+++ src/components/GiftItem.js
@@ -3,32 +3,24 @@ import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function GiftItem({ gift, onGiftSelect }) {
-  const [isHovered, setIsHovered] = useState(true);
-
   return (
-    <div
-      className="gift-item card mb-3"
-      // onMouseEnter={() => setIsHovered(true)}
-      // onMouseLeave={() => setIsHovered(false)}
-    >
+    <div className="gift-item card mb-3">
       <div className="card-header">
         <h3>{gift.name}</h3>
       </div>
-      {isHovered && (
-        <div className="card-body">
-          <img
-            src={gift.imageUrl}
-            alt={gift.name}
-            className="card-img-top hover-image"
-          />
-          <p className="card-text">{gift.description}</p>
-          <Button onClick={() => onGiftSelect(gift.id)} variant="primary">
-            Seleccionar regalo
-          </Button>
-        </div>
-      )}
+      <div className="card-body">
+        <img
+          src={gift.imageUrl}
+          alt={gift.name}
+          className="card-img-top hover-image"
+        />
+        <p className="card-text">{gift.description}</p>
+        <Button onClick={() => onGiftSelect(gift.id)} variant="primary">
+          Seleccionar regalo
+        </Button>
+      </div>
       <div className="card-footer">
-        <p>Quedan: {gift.quantity}</p> {/* Siempre muestra la cantidad */}
+        <p>Quedan: {gift.quantity}</p>
         {gift.quantity === 0 && (
           <p style={{ color: "red", fontWeight: "bold" }}>
             Agotado: Ya no quedan unidades disponibles.
